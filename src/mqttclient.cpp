9637a000e622cfaf7d0d3166ef7e36761a39d5dc
Rudolf Zobel <rudikx6@gmail.com>
2025-08-02 12:26:50 +0200

    setPower auch "0" setzbar um per MQTT zu deaktivieren

--- src/mqttclient.cpp
+++ src/mqttclient.cpp
@@ -42,7 +42,7 @@ void MqttClient::topicCallback(char *topic, byte *payload,
       if (sensor.containsKey("Power")) {
         float power_value = sensor["Power"];
         //power_value += mXy.actualPower();  //deaktiviert für solar überschuss version Rudikx6
-        if (power_value > 0) {
+        if (power_value >= 0) {   // empfangener "0" Wert wird auch geschrieben 
           if (power_value > mCfg.max_power) {
             power_value = mCfg.max_power;
           }
